c3e7e9b4dc3ab4d1d50a3c9475a6dd5c9fcba19f
Ana Raquel <anaraquelpmatos@gmail.com>
2022-02-12 17:36:12 -0300

    Atualizações do chat

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let participantStatus = [];
 let nameParticipant;
+let participantChatInfo = {};
 
-/*let messages = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');*/
 
 function askUser() {
 
-    nameParticipant = {name: prompt("Olá! Qual o seu nome?")};
+    nameParticipant = { name: prompt("Olá! Qual o seu nome?") };
 
     const participants = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", nameParticipant);
 
     participants.then(successRequest);
     participants.catch(notSuccessRequest);
     console.log(nameParticipant);
-    
+
 }
 askUser()
 
@@ -21,7 +20,7 @@ function searchStatus() {
     const statusInChat = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nameParticipant);
     statusInChat.then(successRequest);
     statusInChat.catch(notSuccessRequest);
- 
+
 }
 
 setInterval(searchStatus, 5000)
@@ -35,4 +34,40 @@ function notSuccessRequest(error) {
     console.log(response);
 }
 
+function catchMessages() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promise.then(showChatUpdate);
+    promise.catch(notSuccessRequest);
+
+}
+
+function catchPromise(answer) {
+    console.log(answer);
+    messages = answer.data;
+}
+
+function showChatUpdate(allInfos) {
+    let sender;
+    let recipient;
+    let description;
+    let type;
+    let time;
+
+    for (let i = 0; i < allInfos.data.length; i++) {
+
+        time = allInfos.data[i].time;
+        recipient = allInfos.data[i].to;
+        description = allInfos.data[i].text;
+
+    }
+
+    if (nameParticipant.name) {
+        const ul = document.querySelector("ul");
+        ul.innerHTML += `
+
+  <li><b>(${time})</b> para <b>${recipient}</b>: ${description}</li>
+    
+`;
+    }
+}
 
