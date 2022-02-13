a831afee4816e1f067d1995a2ec0982dca879c78
Ana Raquel <anaraquelpmatos@gmail.com>
2022-02-13 14:10:25 -0300

    Separação por tipo de mensagem

--- script.js
+++ script.js
@@ -42,29 +42,60 @@ function notSuccessRequest(error) {
 }
 
 const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-promise.then((mensagem) => { showChatUpdate(mensagem) });
+promise.then((message) => { showChatUpdate(message) });
 promise.catch(notSuccessRequest);
 
 
 function showChatUpdate(returned) {
 
+
     for (let i = 0; i < returned.data.length; i++) {
 
         let main = document.querySelector("main");
 
-        main.innerHTML += `
+        if (returned.data[i].type == typeStatus) {
+
+            main.innerHTML += `
         
-        <div class="status">
+        <div class="${returned.data.type}">
         <p class="time">(${returned.data[i].time})</p>
         <p class="sender"><b>${returned.data[i].from}</b></p>
-        <p>para</p>
-        <p class="recipient"><b>${returned.data[i].to}</b>:</p>
         <p class="text">${returned.data[i].text}</p>
       </div>`;
 
+        }
+
+        if (returned.data[i].type == typeMessage) {
+
+            main.innerHTML += `
+        
+            <div class="${returned.data.type}">
+            <p class="time">(${returned.data[i].time})</p>
+            <p class="sender"><b>${returned.data[i].from}</b></p>
+            <p>para</p>
+            <p class="recipient"><b>${returned.data[i].to}</b>:</p>
+            <p class="text">${returned.data[i].text}</p>
+          </div>`;
+
+        }
+
+        if (returned.data[i].type == typePrivateMessage) {
+
+            main.innerHTML += `
+        
+            <div class="${returned.data.type}">
+            <p class="time">(${returned.data[i].time})</p>
+            <p class="sender"><b>${returned.data[i].from}</b></p>
+            <p>reservadamente para</p>
+            <p class="recipient"><b>${returned.data[i].to}</b>:</p>
+            <p class="text">${returned.data[i].text}</p>
+          </div>`;
+
+        }
     }
 
-
+    const element = document.querySelector('div');
+    element.scrollIntoView();
 
 }
 
